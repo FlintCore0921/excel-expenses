9146131fdb8bcbf78e166afd85e90fdf76701c59
FlintCore0921 <elioerickramos@gmail.com>
2024-09-30 21:54:14 -0400

    First commit.

---
+++ src/main/java/com/flintcore/excel_expenses/ExcelExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.flintcore.excel_expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelExpensesApplication.class, args);
+	}
+
+}
