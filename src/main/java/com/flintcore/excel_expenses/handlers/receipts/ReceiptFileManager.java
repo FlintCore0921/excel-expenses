c5a8ae3a4021aa870ec91f3b11cfb9c70973fe48
FlintCore0921 <elioerickramos@gmail.com>
2024-10-03 03:27:36 -0400

    refactor-feat: File manager class is abstract and handle default I/O files task.
    feat: Service for Receipt created.

--- src/main/java/com/flintcore/excel_expenses/handlers/receipts/ReceiptFileManager.java
+++ src/main/java/com/flintcore/excel_expenses/handlers/receipts/ReceiptFileManager.java
@@ -13,43 +13,10 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Component
-public class ReceiptFileManager {
-
+public class ReceiptFileManager extends FileManager {
     public static final String[] RECEIPT_FILE_PATH = {"app", "storage", "data", "receipts.txt"};
-    private final ReentrantReadWriteLock lock;
-    private final Lock writeLock;
-    private final Lock readLock;
-
-    // Injected
-    private final SerializeWriter serializeWriter;
-    private final SerializeReader serializeReader;
 
     public ReceiptFileManager(SerializeWriter serializeWriter, SerializeReader serializeReader) {
-        this.serializeWriter = serializeWriter;
-        this.serializeReader = serializeReader;
-        lock = new ReentrantReadWriteLock(true);
-        writeLock = lock.writeLock();
-        readLock = lock.readLock();
-    }
-
-    public <R extends Serializable, L extends Collection<R> & Serializable> void updateReceiptsData(L receipts) {
-        writeLock.lock();
-        try {
-            this.serializeWriter.writeIn(RECEIPT_FILE_PATH, receipts);
-        } finally {
-            writeLock.unlock();
-        }
-    }
-
-    public Set<Receipt> getReceipts() {
-        readLock.lock();
-        HashSet<Receipt> receipts = new HashSet<>();
-        try {
-            Collection<Receipt> read = this.serializeReader.read(RECEIPT_FILE_PATH);
-            receipts.addAll(read);
-        } finally {
-            readLock.unlock();
-        }
-        return receipts;
+        super(RECEIPT_FILE_PATH, serializeWriter, serializeReader);
     }
 }
